e28036537bc7ac1539d9730bdbfe54ea4f6d3a0d
aleDVM <pellahuenco@gmail.com>
2017-07-02 10:08:23 -0400

    Add files via upload
    cambios

---
+++ Experiencia12/Experiencia12/1 metodos/ejercicio4.rb
@@ -0,0 +1,9 @@
+# Crear un metodo que muestre en pantalla un saludo, el metodo
+# debe recibir un parametro, si ese parametro es el string "Hola" el
+# metodo debe mostrar en pantalla "Hola Mundo".
+
+def greet(b)
+  puts 'Hola Mundo' if b == 'Hola'
+end
+
+greet('Hola')
---
+++ Experiencia12/Experiencia12/2 arrays/ejercicio5.rb
@@ -0,0 +1,19 @@
+# Se tiene un arreglo de productos y precios, se pide que el output sea:
+# <div class='product'><p> Producto1 </p><p> Precio: 1000 </p></div>
+# <div class='product'><p> Producto2 </p><p> Precio: 2000 </p></div>
+# <div class='product'><p> Producto3 </p><p> Precio: 1500 </p></div>
+# <div class='product'><p> Producto4 </p><p> Precio: 950 </p></div>
+
+products = %w[Producto1 Producto2 Producto3 Producto4]
+prices = %w[1000 2000 1500 950]
+
+html = ''
+products.each_with_index do |ele, i|
+  a = prices[products.index(ele)]
+
+  html += "<div class='product'>"
+  html += "<p>#{ele}</<p>"
+  html += "<p>#{a}</p>"
+  html += "</div>\n"
+end
+puts html
